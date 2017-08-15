91cdd20dc7b9e181d9680a094a6060b0df269c1b
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 03:34:41 -0700

    Add find_or_create_pokemon

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -28,8 +28,12 @@ defmodule DataHelper do
   def create_from_file(file) do
     data = parse_pokemon(file)
 
-    data
-    |> create_types
+    create_types(data)
+    create_pokemon(data)
+  end
+
+  defp create_pokemon(data) do
+    Enum.map(data, &find_or_create_pokemon/1)
   end
 
   defp create_types(data) do
@@ -55,7 +59,26 @@ defmodule DataHelper do
     Repo.one(from t in Pokemon.Type, where: t.name == ^name) ||
       %Pokemon.Type{}
       |> Pokemon.Type.changeset(%{name: name})
-      |> Repo.insert()
+      |> Repo.insert!
+  end
+
+  defp find_or_create_pokemon(%{name: name} = pokemon) do
+    Repo.one(from p in Pokemon, where: p.name == ^name) ||
+      %Pokemon{}
+      |> Pokemon.changeset(%{
+        attack: pokemon.attack,
+        defense: pokemon.defense,
+        description: pokemon.description,
+        height: pokemon.height,
+        hp: pokemon.hp,
+        name: pokemon.name,
+        number: pokemon.number,
+        special_attack: pokemon.special_attack,
+        special_defense: pokemon.special_defense,
+        speed: pokemon.speed,
+        weight: pokemon.weight
+      })
+      |> Repo.insert!
   end
 end
 
