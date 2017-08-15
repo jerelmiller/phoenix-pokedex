9987cc4da88500930424dd1a486b5420bd4ae34a
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 01:40:30 -0700

    Add height/weight to pokemon

--- priv/repo/migrations/20170815072107_create_pokemons.exs
+++ priv/repo/migrations/20170815072107_create_pokemons.exs
@@ -12,6 +12,8 @@ defmodule Pokedex.Repo.Migrations.CreatePokemons do
       add :special_attack, :integer, null: false
       add :special_defense, :integer, null: false
       add :speed, :integer, null: false
+      add :height, :float, precision: 2, null: false
+      add :weight, :float, precision: 2, null: false
 
       timestamps()
     end
