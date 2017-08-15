c0650b24b2272c319aff4be56e5bec3520cc4e4d
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 01:55:05 -0700

    only serve images from static

--- lib/pokedex_web/endpoint.ex
+++ lib/pokedex_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule PokedexWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :pokedex, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(images)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
