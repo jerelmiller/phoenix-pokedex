f1945e792b05ea0ad6dec281e3a613e9a2d47806
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 04:40:40 -0700

    Add more fields for pokemon

--- lib/pokedex/schema/types.ex
+++ lib/pokedex/schema/types.ex
@@ -3,7 +3,15 @@ defmodule Pokedex.Schema.Types do
 
   object :pokemon do
     field :id, :id
+    field :attack, :integer
+    field :description, :string
+    field :height, :float
+    field :hp, :integer
     field :name, :string
     field :number, :string
+    field :special_attack, :integer
+    field :special_defense, :integer
+    field :speed, :integer
+    field :weight, :float
   end
 end
