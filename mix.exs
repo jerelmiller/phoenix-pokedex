02d4d067000060151e4324a48e881379b2cdf16f
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 00:25:27 -0700

    Add absinthe and absinthe_plug

--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule Pokedex.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.3.2"},
+      {:absinthe_plug, "~> 1.3.1"}
     ]
   end
 
