640208594f50c942b9c957b582d59c1251f5c475
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 03:16:27 -0700

    Start working on inserting seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,43 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Pokedex.{Pokemon, Repo}
+
+defmodule Data.Pokemon do
+  @derive [Poison.Encoder]
+
+  defstruct [
+    :attack, :defense, :description, :evolution, :evolution_chain, :height,
+    :hp, :involution, :moves, :name, :number, :special_attack, :special_defense,
+    :speed, :stamina, :strengths, :types, :weaknesses, :weight
+  ]
+end
+
+defmodule DataHelper do
+  import Ecto.Query
+
+  def parse_pokemon(file) do
+    file
+    |> File.read!()
+    |> Poison.decode!(as: [%Data.Pokemon{}], keys: :atoms)
+  end
+
+  def insert_types(types) do
+    Enum.map(types, &find_or_create_type/1)
+  end
+
+  defp find_or_create_type(name) do
+    Repo.one(from t in Pokemon.Type, where: t.name == ^name) ||
+      %Pokemon.Type{}
+      |> Pokemon.Type.changeset(%{name: name})
+      |> Repo.insert()
+  end
+end
+
+data = DataHelper.parse_pokemon("priv/repo/pokemon.json")
+
+data
+|> Enum.flat_map(fn (pokemon) -> pokemon.types end)
+|> Enum.uniq
+|> DataHelper.insert_types
