18f9a9cc5c750a1b223b00eb5066a7153b005698
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 00:47:16 -0700

    Update pokemon type to use proper belongs_to

--- lib/pokedex/pokemon/pokemon_type.ex
+++ lib/pokedex/pokemon/pokemon_type.ex
@@ -1,11 +1,12 @@
 defmodule Pokedex.Pokemon.PokemonType do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Pokedex.Pokemon.PokemonType
+  alias Pokedex.Pokemon
+  alias Pokedex.Pokemon.{PokemonType, Type}
 
   schema "pokemons_types" do
-    field :pokemon_id, :id
-    field :type_id, :id
+    belongs_to :pokemon, Pokemon
+    belongs_to :type, Type
 
     timestamps()
   end
@@ -13,7 +14,7 @@ defmodule Pokedex.Pokemon.PokemonType do
   @doc false
   def changeset(%PokemonType{} = pokemon_type, attrs) do
     pokemon_type
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:pokemon_id, :type_id])
+    |> validate_required([:pokemon_id, :type_id])
   end
 end
