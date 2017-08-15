5bb41f7ff1ace28d1f1e7d51e901fd403077c450
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 02:11:51 -0700

    Add a pokemon query

--- lib/pokedex/schema.ex
+++ lib/pokedex/schema.ex
@@ -1,7 +1,10 @@
 defmodule Pokedex.Schema do
   use Absinthe.Schema
+  import_types Pokedex.Schema.Types
 
   query do
-
+    field :pokemons, list_of(:pokemon) do
+      resolve &Pokedex.PokemonResolver.all/2
+    end
   end
 end
