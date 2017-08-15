58764852b9ce40b73430bfda893b175c7ec1e44b
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 01:55:59 -0700

    Remove session plug

--- lib/pokedex_web/endpoint.ex
+++ lib/pokedex_web/endpoint.ex
@@ -28,14 +28,6 @@ defmodule PokedexWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_pokedex_key",
-    signing_salt: "ylUyGtZf"
-
   plug PokedexWeb.Router
 
   @doc """
