b88f38cbb5ae9a97138a48f0f82614adda146dae
Jerel Miller <jerelmiller@gmail.com>
2017-08-15 00:48:21 -0700

    Add many_to_many between pokemon and type

--- lib/pokedex/pokemon.ex
+++ lib/pokedex/pokemon.ex
@@ -2,11 +2,14 @@ defmodule Pokedex.Pokemon do
   use Ecto.Schema
   import Ecto.Changeset
   alias Pokedex.Pokemon
+  alias Pokedex.Pokemon.{PokemonType, Type}
 
   schema "pokemons" do
     field :description, :string
     field :name, :string
     field :number, :string
+
+    many_to_many :types, Type, join_through: PokemonType
 
     timestamps()
   end
