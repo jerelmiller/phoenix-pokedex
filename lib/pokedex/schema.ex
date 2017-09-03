74ac115803d89a7d388cff74765864cd9d2ecf22
Jerel Miller <jerelmiller@gmail.com>
2017-09-02 23:41:51 -0700

    Add query to find pokemon by id

--- lib/pokedex/schema.ex
+++ lib/pokedex/schema.ex
@@ -2,9 +2,16 @@ defmodule Pokedex.Schema do
   use Absinthe.Schema
   import_types Pokedex.Schema.Types
 
+  alias Pokedex.Resolvers
+
   query do
     field :pokemons, list_of(:pokemon) do
-      resolve &Pokedex.Resolvers.Pokemon.all/2
+      resolve &Resolvers.Pokemon.all/2
+    end
+
+    field :pokemon, :pokemon do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Pokemon.find/2
     end
   end
 end
