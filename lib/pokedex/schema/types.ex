5b122eb3810499547f898b6f56dd6824b48b2599
Jerel Miller <jerelmiller@gmail.com>
2017-09-02 23:32:03 -0700

    Merge pokemon_moves and moves to better graph data how we think of it

--- lib/pokedex/schema/types.ex
+++ lib/pokedex/schema/types.ex
@@ -2,15 +2,30 @@ defmodule Pokedex.Schema.Types do
   use Absinthe.Schema.Notation
   use Absinthe.Ecto, repo: Pokedex.Repo
 
+  alias Pokedex.{Pokemon, Resolvers}
+
   object :move do
-    field :accuracy, :integer
-    field :effect_chance, :integer
-    field :name, non_null(:string)
-    field :power, :integer
-    field :pp, non_null(:integer)
+    field :level, :integer
+
+    field :accuracy, :integer,
+      resolve: Pokedex.Lookup.path([:move, :accuracy])
+
+    field :effect_chance, :integer,
+      resolve: Pokedex.Lookup.path([:move, :effect_chance])
+
+    field :name, non_null(:string),
+      resolve: Pokedex.Lookup.path([:move, :name])
+
+    field :power, :integer,
+      resolve: Pokedex.Lookup.path([:move, :power])
+
+    field :pp, non_null(:integer),
+      resolve: Pokedex.Lookup.path([:move, :pp])
 
     field :effect, non_null(:string),
-      resolve: Pokedex.Lookup.assoc_lookup(:effect, :description)
+      resolve: Pokedex.Lookup.path([:move, :effect, :description])
+
+    field :move_method, :move_method, resolve: assoc(:move_method)
   end
 
   object :move_method do
@@ -40,14 +55,6 @@ defmodule Pokedex.Schema.Types do
     field :strengths, list_of(:string),
       resolve: Pokedex.Lookup.assoc_lookup(:strengths, :name)
 
-    field :pokemon_moves, list_of(:pokemon_move),
-      resolve: assoc(:pokemon_moves)
-  end
-
-  object :pokemon_move do
-    field :level, non_null(:integer)
-
-    field :move, :move, resolve: assoc(:move)
-    field :move_method, :move_method, resolve: assoc(:move_method)
+    field :moves, list_of(:move), resolve: Resolvers.Pokemon.moves
   end
 end
