57baedb3911006fddd31fdd1a6a2447833334a57
Jerel Miller <jerelmiller@gmail.com>
2017-09-02 22:36:07 -0700

    Add move_method association to pokemon move

--- lib/pokedex/pokemon/pokemon_move.ex
+++ lib/pokedex/pokemon/pokemon_move.ex
@@ -2,13 +2,14 @@ defmodule Pokedex.Pokemon.PokemonMove do
   use Ecto.Schema
   import Ecto.Changeset
   alias Pokedex.Pokemon
-  alias Pokedex.Pokemon.{PokemonMove, Move}
+  alias Pokedex.Pokemon.{PokemonMove, Move, MoveMethod}
 
   schema "pokemons_moves" do
     field :level, :integer
 
     belongs_to :pokemon, Pokemon
     belongs_to :move, Move
+    belongs_to :move_method, MoveMethod
 
     timestamps()
   end
@@ -16,7 +17,7 @@ defmodule Pokedex.Pokemon.PokemonMove do
   @doc false
   def changeset(%PokemonMove{} = pokemon_move, attrs) do
     pokemon_move
-    |> cast(attrs, [:level, :pokemon_id, :move_id])
-    |> validate_required([:level, :pokemon_id, :move_id])
+    |> cast(attrs, [:level, :pokemon_id, :move_id, :move_method_id])
+    |> validate_required([:level, :pokemon_id, :move_id, :move_method_id])
   end
 end
