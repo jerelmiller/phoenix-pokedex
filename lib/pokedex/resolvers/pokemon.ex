b5d05b32c24a103787728b5871d4978261fa98b8
Jerel Miller <jerelmiller@gmail.com>
2017-09-02 23:41:25 -0700

    Add resolver to handle getting a pokemon by id

--- lib/pokedex/resolvers/pokemon.ex
+++ lib/pokedex/resolvers/pokemon.ex
@@ -2,10 +2,18 @@ defmodule Pokedex.Resolvers.Pokemon do
   use Pokedex.Resolver
   use Absinthe.Ecto, repo: Pokedex.Repo
 
+  alias Pokedex.Pokemon
   alias Pokedex.Pokemon.Query
 
   def all(_, _) do
     {:ok, Repo.all(Pokedex.Pokemon)}
+  end
+
+  def find(%{id: id}, _) do
+    case Repo.get(Pokemon, id) do
+      nil -> {:error, "Pokemon #{id} not found"}
+      pokemon -> {:ok, pokemon}
+    end
   end
 
   def moves, do: assoc(:pokemon_moves, &preload_moves/3)
