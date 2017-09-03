f2dfa2d14b2245fabea314fb1a6fe5fa4abf2e94
Jerel Miller <jerelmiller@gmail.com>
2017-09-02 23:30:32 -0700

    Preload move data when fetching moves

--- lib/pokedex/resolvers/pokemon.ex
+++ lib/pokedex/resolvers/pokemon.ex
@@ -2,7 +2,13 @@ defmodule Pokedex.Resolvers.Pokemon do
   use Pokedex.Resolver
   use Absinthe.Ecto, repo: Pokedex.Repo
 
+  alias Pokedex.Pokemon.Query
+
   def all(_, _) do
     {:ok, Repo.all(Pokedex.Pokemon)}
   end
+
+  def moves, do: assoc(:pokemon_moves, &preload_moves/3)
+
+  defp preload_moves(query, _, _), do: Query.preload_moves(query)
 end
