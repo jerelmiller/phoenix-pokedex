1c845b0d2baa37ad5c4c0cef11b7b93e2b702c11
Jerel Miller <jerelmiller@gmail.com>
2017-09-02 20:45:17 -0700

    Define types resolution function to load types for a pokemon

--- lib/pokedex/resolvers/pokemon_resolver.ex
+++ lib/pokedex/resolvers/pokemon_resolver.ex
@@ -1,7 +1,14 @@
 defmodule Pokedex.PokemonResolver do
   use Pokedex.Resolver
+  use Absinthe.Ecto, repo: Pokedex.Repo
 
   def all(_, _) do
     {:ok, Repo.all(Pokedex.Pokemon)}
+  end
+
+  def types(pokemon, _, _) do
+    ecto_batch(Repo, pokemon, :types, fn types ->
+      {:ok, Enum.map(types, fn type -> type.name end)}
+    end)
   end
 end
