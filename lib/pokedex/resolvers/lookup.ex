079e0cc9004da70dc3bf6c0a610a9fc6025713ed
Jerel Miller <jerelmiller@gmail.com>
2017-09-04 01:07:37 -0700

    Generalize assoc_lookup to allow nested paths

--- lib/pokedex/resolvers/lookup.ex
+++ lib/pokedex/resolvers/lookup.ex
@@ -7,16 +7,25 @@ defmodule Pokedex.Resolvers.Lookup do
     fn (parent, _, _) ->
       ecto_batch(Repo, parent, association, fn
         records when is_list(records) ->
-          {:ok, Enum.map(records, &Map.get(&1, field))}
+          {:ok, Enum.map(records, &extract_nested_value(&1, field))}
         record ->
-          {:ok, Map.get(record, field)}
+          {:ok, extract_nested_value(record, field)}
       end)
     end
   end
 
   def path(path) when is_list(path) do
     fn (parent, _, _) ->
-      {:ok, Enum.reduce(path, parent, &Map.fetch!(&2, &1))}
+      {:ok, extract_nested_value(parent, path)}
     end
+  end
+
+  def extract_nested_value(map, path) when is_map(map) and is_list(path) do
+    path
+    |> Enum.reduce(map, &Map.fetch!(&2, &1))
+  end
+
+  def extract_nested_value(map, path) when is_map(map) do
+    extract_nested_value(map, List.wrap(path))
   end
 end
