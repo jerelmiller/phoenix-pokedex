2dc9917c5b8c8cbd76e75740ac9d695f985c29d1
Jerel Miller <jerelmiller@gmail.com>
2018-06-02 16:01:21 -0700

    Dial in some more formatter changes

--- .formatter.exs
+++ .formatter.exs
@@ -2,12 +2,25 @@
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 80,
   locals_without_parens: [
+    # Absinthe
     import_types: 1,
     resolve: 1,
     arg: :*,
     enum: 2,
     object: 2,
     value: 1,
-    field: :*
+    field: :*,
+
+    # Ecto
+    belongs_to: :*,
+    has_many: :*,
+    many_to_many: :*,
+    from: :*,
+
+    # Phoenix
+    transport: 3,
+    plug: :*,
+    socket: :*,
+    render: :*
   ]
 ]
